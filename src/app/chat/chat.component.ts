e932501e5b7ce59e96dd99a670329ae2e1b1446b
Mahesh Kumar <mahesh26.kumar@ril.com>
2020-01-29 12:36:49 +0530

    Paho MQTT integrated

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,24 +1,50 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { ChatService } from '../chat.service';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { Paho } from "../../ng2-mqtt/mqttws31";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit ,OnDestroy{
   @ViewChild("chatMessage",{static:true}) chatMessage:ElementRef
-  messages = ["hi","kya hal chal?","kaise ho ?","nice","game","try something"];
-  constructor(private chatService:ChatService) { }
+  messages = [];
+  private client : Paho.MQTT.Client;
+  private message : Paho.MQTT.Message;
+  constructor() {
+    this.client  = new Paho.MQTT.Client("mqtt.eclipse.org", Number(443), "JioChatMqttclient1")
+    this.client.onConnectionLost = (responseObject: Object) => {
+      console.log('Connection lost.',responseObject);
+    };
+
+    this.client.onMessageArrived = (message: Paho.MQTT.Message) => {
+      this.messages.push(message.payloadString);
+      console.log('Message arrived.', message);
+    };
+
+    this.client.connect({ onSuccess: this.onConnect.bind(this) });
+    }
 
   ngOnInit() {
-    this.chatService.messages.subscribe(msg => {
-      this.messages.push(msg.text);
-    })
   }
-  sendMessage(message: string){
-    this.chatService.sendMsg(message);
-    this.chatMessage.nativeElement.value =""
+  onConnect() { 
+    console.log("onConnect");
+    this.client.subscribe("/JioChatMqtt/topic1",{});
   }
 
+  sendMessage(message:string){
+    if(message!=="") {
+    this.message = new Paho.MQTT.Message(message);
+    this.message.destinationName = "/JioChatMqtt/topic1";
+    this.client.send(this.message);
+    this.chatMessage.nativeElement.value =""
+    }
+    else {
+        alert("Blank message cannot be sent.")
+    }  
+  }
+  ngOnDestroy() {
+    this.client.disconnect();
+  }
+ 
 }
